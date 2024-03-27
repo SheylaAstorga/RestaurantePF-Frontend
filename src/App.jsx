d55c74ee6aaa0d9ac52008fcc193dc5cad4d6583
Javier Jimenez <javier.sniguel@gmail.com>
2024-03-27 14:48:24 -0300

    se agregaron los estilos a la pagina para acomodar la grilla y el detalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Pedido from "./components/Pedido";
+
 function App() {
 
   return (
     <>
-        <h1>Bienvenidos a Sazón del alma</h1>
+      <Pedido></Pedido>
     </>
   )
 }
