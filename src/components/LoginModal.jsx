9f7ffb0b01a5f800ca593cb1713fd8b525552d76
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-27 11:12:04 -0300

    agregue el modal del login y agregue el estilo

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,65 +1,91 @@
 import React from "react";
-import { Container, Button, Form } from "react-bootstrap";
-import imagenlogin from "../../src/img/imagen-login.jpeg";
+import { Button, Form, Modal } from "react-bootstrap";
+import logoSazón from "../../src/img/LogoSazon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const LoginModal = () => {
   const handleLogin = () => {
-    // Lógica para manejar el inicio de sesión
     console.log("Iniciando sesión...");
   };
 
   const handleRegister = () => {
-    // Lógica para manejar el registro
     console.log("Redirigiendo al formulario de registro...");
   };
 
   return (
-    <Container className="container-Login mt-5">
-      <section
-        className="welcome-section text-center"
-        style={{ backgroundImage: `url(${imagenlogin})` }}
-      >
-        <div className="overlay"></div>
-        <div className="content">
-          <h2 className="text-light fs-1 mb-4">Bienvenido</h2>
-          <p>"¡Bienvenido a Sazón del Alma! Disfruta de una experiencia culinaria auténtica y llena de sabor. ¡Inicia sesión para explorar nuestro menú y descubrir los sabores del alma!"</p>
-          <p className="text-light mb-4">
-            ¡Inicia sesión o regístrate para acceder!
-          </p>
-          <Button variant="primary" onClick={handleRegister}>
-            Registrarse
-          </Button>
-        </div>
-      </section>
-      {/* Formulario de inicio de sesión */}
-      <section className="login-section">
-        {/* Botones de inicio de sesión con redes sociales */}
-        <div className="social-login">
-          <Button variant="outline-primary" className="social-button">
-            Iniciar sesión con Facebook
-          </Button>
-          <Button variant="outline-danger" className="social-button">
-            Iniciar sesión con Google
-          </Button>
-        </div>
-        {/* Formulario de inicio de sesión tradicional */}
-        <Form onSubmit={handleLogin}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Correo electrónico"
-              required
+    <div
+      className="modal show"
+      style={{ display: "block", position: "initial" }}
+    >
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <div className="text-center">
+            <img
+              src={logoSazón}
+              alt="logo sazón del alma"
+              className="img-fluid "
+              style={{ width: "200px", height: "auto" }}
             />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Contraseña" required />
-          </Form.Group>
-          <Button variant="success" type="submit">
-            Iniciar sesión
-          </Button>
-        </Form>
-      </section>
-    </Container>
+          </div>
+          <Form onSubmit={handleLogin} className="traditional-login">
+            <Form.Group controlId="formBasicEmail" className="my-4">
+              <Form.Control
+                type="email"
+                placeholder="Correo electrónico"
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Contraseña" required />
+            </Form.Group>
+            <div className="mt-3 checkLogin">
+              <Form.Check type="checkbox" label="Recordar contraseña" />
+            </div>
+            <div className="text-center mt-4">
+              <Button className="btn botonSesion" type="submit">
+                Iniciar sesión
+              </Button>
+            </div>
+            <div className="text-center my-3">
+              <a
+                href="/olvide-contraseña"
+                className="me-5 link-no-underline text-decoration-none linkLoginContraseña"
+              >
+                Olvidé mi contraseña
+              </a>
+              <a
+                href="/olvide-usuario"
+                className="text-decoration-none linkLoginUsuario"
+              >
+                Olvidé mi usuario
+              </a>
+            </div>
+            <div className="social-login text-center">
+              <Button
+                variant="outline-primary"
+                className="social-button me-3 my-2 w-25"
+              >
+                <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+              </Button>
+              <Button variant="outline-danger" className="social-button w-25">
+                <FontAwesomeIcon icon={faGoogle} className="social-icon" />
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <p>¿No tienes una cuenta?</p>
+          <a href="/registro" className="text-decoration-none linkRegistrate">
+            Regístrate aquí
+          </a>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
   );
 };
 
