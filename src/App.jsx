d2514fd0b3539e504a7544016a3ef016ea6a36b6
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-27 18:55:47 -0300

    añadimos maquetado simple para el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import Nav from "./components/common/Nav"
+import Footer from "./components/common/Footer"
 function App() {
 
   return (
     <>
+        <Nav></Nav>
         <h1>Bienvenidos a Sazón del alma</h1>
+        <Footer></Footer>
     </>
   )
 }
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='w-100 py-3'>
+            <p className='text-center text-light m-0'>Aqui pondremos algo mas pero no se que poner</p>
+        </footer>
+    );
+};
+
+export default Footer;
