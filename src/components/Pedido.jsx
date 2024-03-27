1b56ad5818ef8553190bb49874209e012e9451cf
Javier Jimenez <javier.sniguel@gmail.com>
2024-03-27 16:35:47 -0300

    se crearon las rutas de pagar y seguir pidiendo, ademas se agregaron otros pedidos

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -2,6 +2,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import "../style/pedido.css";
 import PedidosIndividuales from "./PedidosIndividuales";
 import Button from "react-bootstrap/Button";
+import { Link } from 'react-router-dom';
 
 const Pedido = () => {
   return (
@@ -9,8 +10,10 @@ const Pedido = () => {
       <h1>Mi pedido</h1>
       <ListGroup className="border-bottom-list">
         <ListGroup.Item>
-          {" "}
-          <PedidosIndividuales></PedidosIndividuales>{" "}
+          <PedidosIndividuales></PedidosIndividuales>
+          <PedidosIndividuales></PedidosIndividuales>
+          <PedidosIndividuales></PedidosIndividuales>
+          <PedidosIndividuales></PedidosIndividuales>
         </ListGroup.Item>
       </ListGroup>
       <article className="d-flex justify-content-between pt-3">
@@ -18,11 +21,13 @@ const Pedido = () => {
         <h3>$8700$</h3>
       </article>
       <article className="d-flex justify-content-end mt-2">
+        <Link to={'/'}>
         <Button variant="outline-secondary" size="lg" className="mx-4">
-          Large button
+          Seguir pidiendo
         </Button>
+        </Link>
         <Button type="submit" size="lg" className="btn_pagar">
-          Large button
+          Pagar
         </Button>
       </article>
     </section>
