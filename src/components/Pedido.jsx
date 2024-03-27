dcd9c4e8e044586164083ad68365f351ba9bafc5
Javier Jimenez <javier.sniguel@gmail.com>
2024-03-27 16:29:57 -0300

    se agregaron los botones y el menu de total a pagar con los respectivos estilos

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,18 +1,32 @@
-import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroup from "react-bootstrap/ListGroup";
 import "../style/pedido.css";
-import PedidosIndividuales from './PedidosIndividuales';
-
+import PedidosIndividuales from "./PedidosIndividuales";
+import Button from "react-bootstrap/Button";
 
 const Pedido = () => {
-    return (
-       
-        <section className="container">
-        <ListGroup className="border-bottom-list">
-          <ListGroup.Item>  <PedidosIndividuales></PedidosIndividuales>  </ListGroup.Item>
-        </ListGroup>
-      </section>
-  
-    );
+  return (
+    <section className="container">
+      <h1>Mi pedido</h1>
+      <ListGroup className="border-bottom-list">
+        <ListGroup.Item>
+          {" "}
+          <PedidosIndividuales></PedidosIndividuales>{" "}
+        </ListGroup.Item>
+      </ListGroup>
+      <article className="d-flex justify-content-between pt-3">
+        <h3>Total a pagar</h3>
+        <h3>$8700$</h3>
+      </article>
+      <article className="d-flex justify-content-end mt-2">
+        <Button variant="outline-secondary" size="lg" className="mx-4">
+          Large button
+        </Button>
+        <Button type="submit" size="lg" className="btn_pagar">
+          Large button
+        </Button>
+      </article>
+    </section>
+  );
 };
 
 export default Pedido;
