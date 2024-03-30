71667bbb353f6371af61f9cc4c4b50f73d820ce9
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-30 11:12:17 -0300

    añado la logica si esta logeado mostrar o no y tambien añadimos los botones

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Menu from "./components/common/Menu"
 import Footer from "./components/common/Footer"
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
 function App() {
   const usuario = JSON.parse(sessionStorage.getItem("usuarioSazonDelAlma")) || "";
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,52 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
-const Menu = () => {
+const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
+    const navegacion = useNavigate()
+    const logout = () => {
+        sessionStorage.removeItem("usuarioSazonDelAlma");
+        setUsuarioLogueado("");
+        navegacion("/");
+    };
     return (
-        <div>
-            
-        </div>
+        <Navbar expand="lg" className="NavFondo">
+            <Container className="d-flex justify-content-between">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="">
+                        <NavLink end className="nav-link text-light" to="/">
+                            Inicio
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src=""
+                        alt="[Texto alternativo del logo]"
+                        className="img-fluid text-light"
+                    />
+                </Navbar.Brand>
+                {usuarioLogueado !== "" ? (
+                    <>
+                        <NavLink end className="nav-link text-light" to="/administrador">
+                            Administrador
+                        </NavLink>
+                        <Button className="nav-link text-light" variant="danger" onClick={logout}>
+                            logout
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        <NavLink end className="nav-link text-light btn colorBoton2 me-1" to="/registro">
+                            <i className="bi bi-person-fill-add fs-3 px-2"></i>
+                        </NavLink>
+                        <NavLink end className="nav-link text-light btn colorBoton1" to="/login">
+                            <i className="bi bi-person-fill fs-3 px-2"></i>
+                        </NavLink>
+                    </>
+                )}
+            </Container>
+        </Navbar>
     );
 };
 
