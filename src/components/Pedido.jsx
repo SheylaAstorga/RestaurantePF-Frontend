cca8067106a54f19dd2cc04ef9aa50901487a4ea
Javier Jimenez <javier.sniguel@gmail.com>
2024-04-01 21:19:37 -0300

    se realizo el estilo responsive agregando cambios en la posicion de los botones como columnas

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,12 +1,12 @@
-import ListGroup from "react-bootstrap/ListGroup";
 import "../style/pedido.css";
+import ListGroup from "react-bootstrap/ListGroup";
 import PedidosIndividuales from "./PedidosIndividuales";
 import Button from "react-bootstrap/Button";
 import { Link } from 'react-router-dom';
 
 const Pedido = () => {
   return (
-    <section className="container">
+    <section className="container c-principal">
       <article className="d-flex justify-content-between">
       <h1>Mi pedido</h1>
       <Button variant="link" className="m-3">Limpiar</Button>
@@ -23,9 +23,9 @@ const Pedido = () => {
         <h3>Total a pagar</h3>
         <h3>$8700$</h3>
       </article>
-      <article className="d-flex justify-content-end mt-2">
+      <article className="group-pagar d-flex justify-content-end mt-2">
         <Link to={'/'}>
-        <Button variant="outline-secondary" size="lg" className="mx-4 btn-Seguir">
+        <Button variant="outline-secondary" size="lg" className="btn-Seguir">
           Seguir pidiendo
         </Button>
         </Link>
