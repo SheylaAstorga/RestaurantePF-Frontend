067123babd813adb1d6aaa20f07dc30480383c2a
Natalia Morales <natii.vmorales98@gmail.com>
2024-04-02 17:56:33 -0300

    se agrego el archivo acercaDeNosotros.css con los respectivos estilos del componente AcercaDeNosotros

--- src/components/pages/AcercaDeNosotros.jsx
+++ src/components/pages/AcercaDeNosotros.jsx
@@ -1,81 +1,82 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
+import "../../style/acercaDeNosotros.css";
 
 const AcercaDeNostros = () => {
   return (
-    <section className="bg-primary">
+    <section className="colorFondoAN">
       <Container>
-        <h2>Nuestro equipo</h2>
-        <Row className="justify-content-center">
+        <h2 className="colorTituloAN text-center pt-5">Nuestro equipo</h2>
+        <Row className="justify-content-center py-5">
           <Col xs={10} md={6} lg={4} className="py-2">
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card className="text-center colorFondoCardAN colorLetraCardAN p-3">
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" className=""/>
               <Card.Body>
                 <Card.Title>Nombre</Card.Title>
                 <Card.Link href="#">
-                  <i className="bi bi-linkedin"></i>
+                  <i className="bi bi-linkedin colorIconoCardAN"></i>
                 </Card.Link>
                 <Card.Link href="#">
-                  <i className="bi bi-github"></i>
+                  <i className="bi bi-github colorIconoCardAN"></i>
                 </Card.Link>
               </Card.Body>
             </Card>
           </Col>
 
           <Col xs={10} md={6} lg={4} className="py-2">
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card className="text-center colorFondoCardAN colorLetraCardAN p-3">
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" className=""/>
               <Card.Body>
                 <Card.Title>Nombre</Card.Title>
                 <Card.Link href="#">
-                  <i className="bi bi-linkedin"></i>
+                  <i className="bi bi-linkedin colorIconoCardAN"></i>
                 </Card.Link>
                 <Card.Link href="#">
-                  <i className="bi bi-github"></i>
+                  <i className="bi bi-github colorIconoCardAN"></i>
                 </Card.Link>
               </Card.Body>
             </Card>
           </Col>
 
           <Col xs={10} md={6} lg={4} className="py-2">
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card className="text-center colorFondoCardAN colorLetraCardAN p-3">
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" className=""/>
               <Card.Body>
                 <Card.Title>Nombre</Card.Title>
                 <Card.Link href="#">
-                  <i className="bi bi-linkedin"></i>
+                  <i className="bi bi-linkedin colorIconoCardAN"></i>
                 </Card.Link>
                 <Card.Link href="#">
-                  <i className="bi bi-github"></i>
+                  <i className="bi bi-github colorIconoCardAN"></i>
                 </Card.Link>
               </Card.Body>
             </Card>
           </Col>
 
           <Col xs={10} md={6} lg={4} className="py-2">
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card className="text-center colorFondoCardAN colorLetraCardAN p-3">
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" className=""/>
               <Card.Body>
                 <Card.Title>Nombre</Card.Title>
                 <Card.Link href="#">
-                  <i className="bi bi-linkedin"></i>
+                  <i className="bi bi-linkedin colorIconoCardAN"></i>
                 </Card.Link>
                 <Card.Link href="#">
-                  <i className="bi bi-github"></i>
+                  <i className="bi bi-github colorIconoCardAN"></i>
                 </Card.Link>
               </Card.Body>
             </Card>
           </Col>
 
           <Col xs={10} md={6} lg={4} className="py-2">
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card className="text-center colorFondoCardAN colorLetraCardAN p-3">
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" className=""/>
               <Card.Body>
                 <Card.Title>Nombre</Card.Title>
                 <Card.Link href="#">
-                  <i className="bi bi-linkedin"></i>
+                  <i className="bi bi-linkedin colorIconoCardAN"></i>
                 </Card.Link>
                 <Card.Link href="#">
-                  <i className="bi bi-github"></i>
+                  <i className="bi bi-github colorIconoCardAN"></i>
                 </Card.Link>
               </Card.Body>
             </Card>
