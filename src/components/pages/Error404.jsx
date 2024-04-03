4bad4be3d02409b41f4b29f7e269604324198100
Javier Jimenez <javier.sniguel@gmail.com>
2024-04-02 21:15:57 -0300

    se cambio el color de boton y se alinearon los componentes

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -8,7 +8,7 @@ const Error404 = () => {
         <div className='text-center contenedor'>
             <img src={error} alt="error 404" />
             <h1>Oops! La pagina no funciona</h1>
-            <Button variant='primary'>Volver al inicio</Button>{' '}
+            <Button variant="primary">Primary</Button>{' '}
         </div>
     );
 };
