4090287de27451b203b22d1c736bf05952c42780
Javier Jimenez <javier.sniguel@gmail.com>
2024-04-02 21:24:29 -0300

    se actualizo los estilos para cambiar el boton y el ancho de la imagen para los dispositivos

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -8,7 +8,7 @@ const Error404 = () => {
         <div className='text-center contenedor'>
             <img src={error} alt="error 404" />
             <h1>Oops! La pagina no funciona</h1>
-            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="primary">Volver ala inicio</Button>{' '}
         </div>
     );
 };
