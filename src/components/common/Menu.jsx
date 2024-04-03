a4b4e21bb712c23c07037b65febf83e28b62b338
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-03 17:11:45 -0300

    solucionamos los errores de los widht del nav

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -10,13 +10,41 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
         setUsuarioLogueado("");
         navegacion("/");
     };
-    // modificar el como se centran los elementos haciendo que el boton hamburguesa a la izquierda , el logo en el centro y el iniciar secion y registrar al final
+    // en la parte de los link hay mucho espacio
     return (
         <Navbar expand="lg" className="NavFondo">
-            <Container className="d-flex justify-content-between align-items-center">
+            <Container className="d-flex">
                 {/* cambiar de color a el boton hamburgesa */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Brand className="m-0 d-flex justify-content-start logo-brand order-lg-4" as={Link} to="/">
+                    <img
+                        src={logo}
+                        alt="logo"
+                        className="text-light logoNav"
+                    />
+                </Navbar.Brand>
+                <div className="d-flex order-lg-5">
+                    {usuarioLogueado !== "" ? (
+                        <>
+                            <NavLink end className="nav-link text-light" to="/administrador">
+                                Administrador
+                            </NavLink>
+                            <Button className="nav-link text-light" variant="danger" onClick={logout}>
+                                <i className="bi bi-person-fill-x fs-3 px-2"></i>
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink end className="nav-link text-light btn colorBoton2 me-1" to="/registro" title="Registrar">
+                                <i className="bi bi-person-fill-add fs-3 px-2"></i>
+                            </NavLink>
+                            <NavLink end className="nav-link text-light btn colorBoton1" to="/login" title="Iniciar Secion">
+                                <i className="bi bi-person-fill fs-3 px-2"></i>
+                            </NavLink>
+                        </>
+                    )}
+                </div>
+                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-3 links-nav">
                     {/* modificar todas las rutas cuando este terminado el frontend */}
                     <Nav className="">
                         <NavLink end className="nav-link text-light footerTitulos" to="/">
@@ -30,32 +58,6 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
                         </NavLink>
                     </Nav>
                 </Navbar.Collapse>
-                <Navbar.Brand className="m-0" as={Link} to="/">
-                    <img
-                        src={logo}
-                        alt="logo"
-                        className="text-light w-25"
-                    />
-                </Navbar.Brand>
-                {usuarioLogueado !== "" ? (
-                    <>
-                        <NavLink end className="nav-link text-light" to="/administrador">
-                            Administrador
-                        </NavLink>
-                        <Button className="nav-link text-light" variant="danger" onClick={logout}>
-                            <i className="bi bi-person-fill-x fs-3 px-2"></i>
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <NavLink end className="nav-link text-light btn colorBoton2 me-1" to="/registro" title="Registrar">
-                            <i className="bi bi-person-fill-add fs-3 px-2"></i>
-                        </NavLink>
-                        <NavLink end className="nav-link text-light btn colorBoton1" to="/login" title="Iniciar Secion">
-                            <i className="bi bi-person-fill fs-3 px-2"></i>
-                        </NavLink>
-                    </>
-                )}
             </Container>
         </Navbar>
     );
