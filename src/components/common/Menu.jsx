6e6b06e8975440d259a0b981e2717cb2c7538b3a
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-04 19:15:39 -0300

    solucionamos el problema del logo

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -12,16 +12,16 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
     };
     return (
         <Navbar expand="lg" className="NavFondo" variant="dark">
-            <Container className="d-flex justify-content-around">
+            <div className="container contenedor-navbar">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
-                <Navbar.Brand className="m-0 d-flex justify-content-start logo-brand order-lg-4 flex-grow-1" as={Link} to="/">
+                <Navbar.Brand className="m-0 d-flex justify-content-center logo-brand order-lg-4 flex-grow-1" as={Link} to="/">
                     <img
                         src={logo}
                         alt="logo"
                         className="text-light logoNav"
                     />
                 </Navbar.Brand>
-                <div className="d-flex order-lg-5 flex-grow-0">
+                <div className="d-flex order-lg-5 flex-grow-0 botones-seciones justify-content-end">
                     {usuarioLogueado !== "" ? (
                         <>
                             <NavLink end className="nav-link text-light" to="/administrador">
@@ -42,7 +42,7 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
                         </>
                     )}
                 </div>
-                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-3 links-nav flex-grow-1">
+                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-3 links-nav flex-grow-0">
                     {/* modificar todas las rutas cuando este terminado el frontend */}
                     <Nav className="me-auto">
                         <NavLink end className="nav-link footerTitulos" to="/">
@@ -56,7 +56,7 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
                         </NavLink>
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
+            </div>
         </Navbar>
     );
 };
