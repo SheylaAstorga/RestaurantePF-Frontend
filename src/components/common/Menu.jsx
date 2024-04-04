d51cf844eb7d24826c1702442f7ab67047610d6f
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-04 18:15:11 -0300

    quitamos comentarios

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -10,20 +10,18 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
         setUsuarioLogueado("");
         navegacion("/");
     };
-    // en la parte de los link hay mucho espacio
     return (
         <Navbar expand="lg" className="NavFondo" variant="dark">
-            <Container className="d-flex">
-                {/* cambiar de color a el boton hamburgesa */}
+            <Container className="d-flex justify-content-around">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
-                <Navbar.Brand className="m-0 d-flex justify-content-start logo-brand order-lg-4" as={Link} to="/">
+                <Navbar.Brand className="m-0 d-flex justify-content-start logo-brand order-lg-4 flex-grow-1" as={Link} to="/">
                     <img
                         src={logo}
                         alt="logo"
                         className="text-light logoNav"
                     />
                 </Navbar.Brand>
-                <div className="d-flex order-lg-5">
+                <div className="d-flex order-lg-5 flex-grow-0">
                     {usuarioLogueado !== "" ? (
                         <>
                             <NavLink end className="nav-link text-light" to="/administrador">
@@ -44,9 +42,9 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
                         </>
                     )}
                 </div>
-                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-3 links-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-3 links-nav flex-grow-1">
                     {/* modificar todas las rutas cuando este terminado el frontend */}
-                    <Nav className="">
+                    <Nav className="me-auto">
                         <NavLink end className="nav-link footerTitulos" to="/">
                             <i className="bi bi-house"></i> Inicio
                         </NavLink>
