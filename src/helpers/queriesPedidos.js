0affc04930ec7c6487db7b88fbd41f86b796da75
Natalia Morales <natii.vmorales98@gmail.com>
2024-04-11 01:36:11 -0300

    se agrego el archivo queriesPedidos.js

---
+++ src/helpers/queriesPedidos.js
@@ -0,0 +1,11 @@
+
+
+export const leerPedidosAPI = () => {
+    try {
+        // const respuesta = await fetch(api_pedidos);
+        // const listaPedidos = await respuesta.json;
+        // return listaPedidos;
+    } catch (error) {
+        console.log(error);
+    }
+}
