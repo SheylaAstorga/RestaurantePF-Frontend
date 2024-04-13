cb210b14c0ef4684607cd306874a61a37807d936
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-13 11:02:16 -0300

    añadimos las funciones y maquetado de los sweetalert, tambien añadimos el responsive

--- src/components/paginaAdministrador/UsuariosAdmin.jsx
+++ src/components/paginaAdministrador/UsuariosAdmin.jsx
@@ -3,17 +3,82 @@ import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { leerUsuarios } from "../../helpers/queris";
 import UsuariosItem from "./UsuariosItem";
+import Swal from "sweetalert2";
 
 const UsuariosAdmin = () => {
     const [usuarios, setUsuarios] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         hacerSolicitud()
-    },[])
+    }, [])
 
-    const hacerSolicitud = async() => {
+    const hacerSolicitud = async () => {
         const respuesta = await leerUsuarios();
         console.log(respuesta)
         setUsuarios(respuesta)
+    }
+
+    const borrarUsuario = async (email) => {
+        Swal.fire({
+            title: "¿Seguro quieres borrarlo?",
+            text: "el usuario se eliminaria permanentemente",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3e3f3d",
+            confirmButtonText: "borrar",
+            cancelButtonText: "cancelar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Usuario Borrado",
+                text: `el usuario ${email} a sido borrado`,
+                icon: "success"
+              });
+            }
+          });
+    }
+
+    const suspenderUsuario = async (email) => {
+      Swal.fire({
+        title: "¿Seguro quieres suspender el usuario?",
+        text: "el usuario no podra hacer ninguna accion al menos que lo habilites",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3e3f3d",
+        confirmButtonText: "suspender",
+        cancelButtonText: "cancelar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Usuario Suspendido",
+            text: `el usuario ${email} a sido suspendido`,
+            icon: "success"
+          });
+        }
+      });
+    }
+
+    const habilitarUsuario = async (email) => {
+        Swal.fire({
+          title: "¿Seguro quieres habilitar el usuario?",
+          text: "el usuario podra hacer pedidos y demas opciones otra ves",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#4baf23",
+          cancelButtonColor: "#3e3f3d",
+          confirmButtonText: "habilitar",
+          cancelButtonText: "cancelar"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire({
+              title: "Usuario Habilitado",
+              text: `el usuario ${email} a sido habilitado`,
+              icon: "success"
+            });
+          }
+        });
+
     }
     return (
         <Container className="my-3">
@@ -27,7 +92,13 @@ const UsuariosAdmin = () => {
             {
                 usuarios.length === 0 ? (<p className="my-3 text-center">No se encontraron usuarios</p>) : (
                     <div className="my-3">
-                        {usuarios.map((usuario, posicion) => <UsuariosItem usuario={usuario} key={posicion}></UsuariosItem>)}
+                        {usuarios.map((usuario, posicion) => 
+                             <UsuariosItem 
+                                borrarUsuario={borrarUsuario}
+                                habilitarUsuario={habilitarUsuario}
+                                suspenderUsuario={suspenderUsuario}
+                                usuario={usuario} 
+                                key={posicion}></UsuariosItem>)}
                     </div>
                 )
             }
--- src/components/paginaAdministrador/UsuariosItem.jsx
+++ src/components/paginaAdministrador/UsuariosItem.jsx
@@ -1,33 +1,36 @@
 import { Button } from "react-bootstrap";
 import "../../style/usuarios.css"
 
-const UsuariosItem = ({ usuario }) => {
+const UsuariosItem = ({ usuario, borrarUsuario, suspenderUsuario, habilitarUsuario }) => {
     const estiloColor = {
         backgroundColor: usuario.perfilRGB,
         margin: "auto",
     };
-
-    const estiloActivo = {
+    const isSuspendido = usuario.suspendido ?
+     (<Button className="ms-lg-3 ms-md-3 mt-2" onClick={()=>habilitarUsuario(usuario.email)} variant="warning"><i className="bi bi-ban"></i> habilitar</Button>) : 
+     (<Button className="ms-lg-3 ms-md-3 mt-2" onClick={()=>suspenderUsuario(usuario.email)} variant="info"><i className="bi bi-ban"></i> Suspender</Button>);
+    
+     const estiloActivo = {
         backgroundColor: usuario.isActive ? "#4baf23" : "#3e3f3d"
     };
     const activo = usuario.isActive ? "activo" : "inactivo"
 
     return (
-        <div className='d-flex w-100 border border-black mb-4 py-4'>
-            <div className="ms-4 w-25">
+        <div className='d-flex w-100 border border-black mb-4 py-4 containerUsuario'>
+            <div className="ms-4 w-lg-25 w-sm-100 w-md-25">
                 <div className="usuarioIsActivo rounded-circle position-absolute" title={activo} style={estiloActivo}></div>
                 <div className='usuarioPerfil rounded-circle position-relative' style={estiloColor}></div>
             </div>
-            <div className='w-75 ps-4'>
+            <div className='ps-4 w-lg-75 w-sm-100 w-md-75 flex-grow-1 pe-2'>
                 <div className="d-flex">
                     <h4 className="m-0">{usuario.nombreUsuario}</h4>
                     {usuario.suspendido ? (<p className="text-danger mt-2 mb-0 ms-2"><b>Usuario suspendido</b></p>) : (<></>)}
                 </div>
-                <p>{usuario.email}</p>
+                <p className="email">{usuario.email}</p>
                 <hr />
                 <div className="d-flex botonesUsuario">
-                    <Button variant="danger"><i className="bi bi-trash3"></i> Borrar</Button>
-                    <Button className="ms-lg-3 ms-md-3" variant="info"><i className="bi bi-ban"></i> Suspender</Button>
+                    <Button onClick={()=>borrarUsuario(usuario.email)} className="mt-2" variant="danger"><i className="bi bi-trash3"></i> Borrar</Button>
+                    {isSuspendido}
                 </div>
             </div>
         </div>
