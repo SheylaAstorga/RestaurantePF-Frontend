07fe1544ff617b094dc57edd1f63f70906a64ed8
sheyla astorga <sheylaastorga1998@gmail.com>
2024-04-17 10:55:40 -0300

    se modifico el redireccionamiento del boton de la pagina error404 para que me lleve a la pagina principal

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "../../style/error404.css";
 import error from "../../assets/error.png";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
     <div className="text-center contenedor">
       <img src={error} alt="error 404" />
       <h1>Oops! La pagina no funciona</h1>
-      <Button variant="primary" className="mb-4 mt-4">
+      <Link to="/"  className="mb-4 mt-4 btn btn-primary">
         Volver al Inicio
-      </Button>{" "}
+      </Link>
     </div>
   );
 };
