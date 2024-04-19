e7e1029d01964eda0a8ba9ad3c193bc859b42e3c
Javier Jimenez <javier.sniguel@gmail.com>
2024-04-18 22:35:16 -0300

    se agregaron los import de las alertas

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -11,6 +11,8 @@ import "../../style/detalleProducto.css";
 import ItemDetalle from "./ItemDetalle";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 import {
   crearPedidoAPI,
